10d092d111a4cb2ce7b8c934e79973d0af15c317
Максим <changehwid518@gmail.com>
2025-07-19 18:54:58 +0300

    БД бота

--- database.py
+++ database.py
@@ -7,11 +7,37 @@ def init_db():
         CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY,
             fi TEXT NOT NULL,
-            subgroup TEXT
+            subgroup TEXT NOT NULL
         )
     ''')
     conn.commit()
     conn.close()
+
+def add_to_queue(user_id, fi, subgroup, lab_number):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+        # Создаём таблицу, если её ещё нет
+    cursor.execute('''CREATE TABLE IF NOT EXISTS queue (
+            user_id INTEGER,
+            fi TEXT,
+            subgroup TEXT,
+            lab_number TEXT
+                        )''')
+
+# Проверяем, не записан ли пользователь уже на эту лабораторную
+    cursor.execute("SELECT * FROM queue WHERE user_id = ? AND lab_number = ?", (user_id, lab_number))
+    if cursor.fetchone():
+        conn.close()
+        return False  # Уже есть запись
+
+# Добавляем пользователя в очередь
+    cursor.execute("INSERT INTO queue (user_id, fi, subgroup, lab_number) VALUES (?, ?, ?, ?)",
+                       (user_id, fi, subgroup, lab_number))
+
+    conn.commit()
+    conn.close()
+    return True
 
 def add_user(user_id, fi):
     conn = sqlite3.connect('users.db')
